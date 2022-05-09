83758db9e6a90de32c6dd067fc9e5b2195afa2df
Mateo <mtaycruz@gmail.com>
2022-05-09 12:14:59 -0400

    images and files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <section className="hero">
+        <div className='hero-logo'>
+            <h3>Hi, Im</h3>
+            <h1>Mateo Cruz</h1>
+        </div>
+    </section>
+  )
+}
+
